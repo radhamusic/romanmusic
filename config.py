c9f2bf905148fb2690f6fdb3008a99f481fb52ab
radhamusic <radhamusic5911@gmail.com>
2024-11-21 22:18:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Iamvillain77/SONA",
+    "https://github.com/radhamusic/arshmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
